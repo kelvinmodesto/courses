c4a2b30f7646d81b26dcc4e27997aed216f522ae
Kelvin Modesto <km.modesto@gmail.com>
2018-07-15 03:42:13 -0300

    Correct window variables

--- udacity/frontend/frogger/src/assets/js/app.js
+++ udacity/frontend/frogger/src/assets/js/app.js
@@ -3,6 +3,23 @@ var Character = function() {
 
 };
 
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Character.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Character.prototype.handleInput = function() {
+
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -10,7 +27,7 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'assets/images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -30,28 +47,42 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player =  function() {
-
+    this.pathImages = `assets/images/`;
+    this.characters =   [
+                        `${this.pathImages}char-boy.png`,
+                        `${this.pathImages}char-cat-girl.png`,
+                        `${this.pathImages}char-horn-girl.png`,
+                        `${this.pathImages}char-pink-girl.png`,
+                        `${this.pathImages}char-princess-girl.png`
+                        ];
 };
 
 Player.prototype.update = function(dt) {
-
 };
 
 Player.prototype.render = function() {
-
 };
 
 Player.prototype.handleInput = function() {
 
 };
 
+Player.prototype.chooseCharacter = function() {
+
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+window.allEnemies = new Array();
+//console.log(enemy);
 
+window.player =  new Player();
+//console.log(player);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
