f57f58390cfd4401b24f0007ce89972e5d82dbf5
Kelvin Modesto <km.modesto@gmail.com>
2018-07-15 02:48:30 -0300

    Rollback to ES5

--- udacity/frontend/frogger/src/assets/js/app.js
+++ udacity/frontend/frogger/src/assets/js/app.js
@@ -1,72 +1,54 @@
-export class Character {
-    constructor(sprite) {
-        this.sprite=sprite;
-    }
+// Using father class
+var Character = function() {
 
-    update(dt){
-
-    }
-
-    render(){
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-}
+};
 
 // Enemies our player must avoid
-export class Enemy extends Character{
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    constructor(sprite) {
-        super(sprite);
-    }
+    this.sprite = 'images/enemy-bug.png';
+};
 
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
-    update(dt){
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-        super.update(dt);
-    }
+};
 
-    // Draw the enemy on the screen, required method for game
-    render(){
-        super.render();
-    }
-}
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player =  function() {
 
-export class Player extends Character {
-    constructor(sprite) {
-        super(sprite);
-    }
+};
 
-    update(){
+Player.prototype.update = function(dt) {
 
-    }
+};
 
-    render(){
-        super.render();
-    }
+Player.prototype.render = function() {
 
-    handleInput(){
+};
 
-    }
+Player.prototype.handleInput = function() {
 
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
- 
-// let enemy = new Enemy('assets/images/enemy-bug.png');
-let player = new Player();
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
