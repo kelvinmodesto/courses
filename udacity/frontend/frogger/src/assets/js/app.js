bbc46d6419948979feb58f942c65efb80fdbd1a4
Kelvin Modesto <km.modesto@gmail.com>
2018-07-16 12:55:07 -0300

    Refactoring for ES6 classes

--- udacity/frontend/frogger/src/assets/js/app.js
+++ udacity/frontend/frogger/src/assets/js/app.js
@@ -1,94 +1,100 @@
-// Using father class
-const Character = function() {
+﻿class Character {
+    constructor(x,y,sprite) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+        this.x = x;
+        this.y = y;
+        this.sprite= sprite;
+    }
 
-};
+    update() {
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Character.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    }
 
-// Draw the enemy on the screen, required method for game
-Character.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
-Character.prototype.handleInput = function() {
-
-};
+}
 
 // Enemies our player must avoid
-const Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy extends Character {
+    constructor(x,y) {
+        super(x,y,`assets/images/enemy-bug.png`);
+        this.speed = Math.floor(Math.random()*(250-50+1));
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'assets/images/enemy-bug.png';
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        this.x += this.speed*dt;
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    render() {
+        super.render();
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player =  function() {
+class Player extends Character {
+    constructor(x,y,sprite) {
+        super(x,y,sprite);
+        this.pathImages = `assets/images/`;
+        this.characters =   [
+                            `${this.pathImages}char-boy.png`,
+                            `${this.pathImages}char-cat-girl.png`,
+                            `${this.pathImages}char-horn-girl.png`,
+                            `${this.pathImages}char-pink-girl.png`,
+                            `${this.pathImages}char-princess-girl.png`
+                            ];
+        // this.sprite = this.characters[this.chooseCharacter()];
+    }
 
-    this.pathImages = `assets/images/`;
+    update() {
 
-    this.characters =   [
-                        `${this.pathImages}char-boy.png`,
-                        `${this.pathImages}char-cat-girl.png`,
-                        `${this.pathImages}char-horn-girl.png`,
-                        `${this.pathImages}char-pink-girl.png`,
-                        `${this.pathImages}char-princess-girl.png`
-                        ];
-};
+    }
 
-Player.prototype.update = function(dt) {
+    render() {
+        super.render();
+    }
 
-};
+    handleInput() {
+    }
 
-Player.prototype.render = function() {
+    chooseCharacter() {
+        return Math.floor(Math.random()*(5+1))
+    }
 
-};
-
-Player.prototype.handleInput = function() {
-
-};
-
-Player.prototype.chooseCharacter = function() {
-
-};
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const allEnemies = [];
-//console.log(enemy);
+const allEnemies = []; 
+window.setInterval(
+    () => { 
+        const e = new Enemy(0,50);
+        const e2 = new Enemy(0,133);
+        const e3 = new Enemy(0,216);
+        allEnemies.push(e);
+        allEnemies.push(e2);
+        allEnemies.push(e3);
+        e.render; 
+        e2.render;
+        e3.render;
+    },5000
+);
 
-const player =  new Player();
-//console.log(player);
+const player =  new Player(200,382,`assets/images/char-boy.png`);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
