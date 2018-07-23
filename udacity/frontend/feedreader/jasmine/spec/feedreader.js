19935d86dfbbccd00e85481078f670eea68bee9e
Kelvin <km.modesto@gmail.com>
2018-07-23 17:03:42 -0300

    Using Arrow functions

--- udacity/frontend/feedreader/jasmine/spec/feedreader.js
+++ udacity/frontend/feedreader/jasmine/spec/feedreader.js
@@ -9,11 +9,15 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
+
+    beforeEach( () => {
+        
+    });
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +25,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,9 +35,9 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('URL is not empty', function () {
-            allFeeds.forEach((feed)=>{
-               expect(feed.url).toBeDefined()
+        it('URL is not empty', () => {
+            allFeeds.forEach((feed)=> {
+               expect(feed.url).toBeDefined();
                expect(feed.url.length).not.toBe(0);
              });
         });
@@ -43,9 +47,9 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('name is not empty', function () {
-            allFeeds.forEach((feed)=>{
-               expect(feed.name).toBeDefined()
+        it('name is not empty', () => {
+            allFeeds.forEach((feed)=> {
+               expect(feed.name).toBeDefined();
                expect(feed.name.length).not.toBe(0);
             });
         });
@@ -53,49 +57,50 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
-    describe('The menu', function () {
+    describe('The menu', () => {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it('', function () {
+        // it('', () => {
              
-        });
+        // });
+        
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-        it('', function (done) {
+        // it('', () => {
             
-        });
+        // });
 
 
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe('Initial Entries', function () {
+    describe('Initial Entries', () => {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        it('', function (done) {
+        // it('', () => {
             
-        });
+        // });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function () {
+    describe('New Feed Selection', () => {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        it('', function (done) {
+        // it('', () => {
             
-        });
+        // });
     });
+
 }());
