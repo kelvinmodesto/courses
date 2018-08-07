fbcdbac00cea3d913ddd3122cf07423a0fd5228e
Kelvin <km.modesto@gmail.com>
2018-08-07 11:54:12 -0300

    Update checkboxes.js

--- udacity/frontend/examples/ud891-gh-pages/lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ udacity/frontend/examples/ud891-gh-pages/lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -20,7 +20,13 @@
     this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
     this.el.addEventListener('click', this.toggle.bind(this));
 
-    // Any other set-up we want to do here?
+    // Initialize role and aria-checked state.
+    this.el.setAttribute('role', 'checkbox');
+    if (this.el.hasAttribute('checked')) {
+      this.el.setAttribute('aria-checked', 'true');
+    } else {
+      this.el.setAttribute('aria-checked', 'false');
+    }
   }
 
   Checkbox.prototype.handleKeyDown = function(e) {
@@ -36,14 +42,14 @@
   Checkbox.prototype.toggle = function() {
     if (this.el.hasAttribute('checked')) {
       this.el.removeAttribute('checked');
-
-      // Hmm.
-
+      
+      // Keep checked attribute and aria-checked in sync.
+      this.el.setAttribute('aria-checked', 'false');
     } else {
       this.el.setAttribute('checked', '');
 
-      // Hmmmmm.
-
+      // Keep checked attribute and aria-checked in sync.
+      this.el.setAttribute('aria-checked', 'true');
     }
   };
 
