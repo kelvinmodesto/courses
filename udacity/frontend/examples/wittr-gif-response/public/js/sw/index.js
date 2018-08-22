2e28542bb11bf762d9bd2f2726a0a7537f1994ae
Kelvin Modesto <km.modesto@gmail.com>
2018-08-22 20:27:34 -0300

    Clean wittr projects

--- udacity/frontend/examples/wittr-gif-response/public/js/sw/index.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  if(event.request.url.endsWith('.jpg')) {
-	  event.respondWith(
-	    fetch('/imgs/dr-evil.gif')
-	  );
-  }
-});
